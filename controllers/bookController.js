a851698bf38269349b388dc8d2eeb9b579d18ddf
Jason Ozulumba <cjozulumba@gmail.com>
2020-11-07 17:16:17 -0500

    Setup of Book CRUD routes

---
+++ controllers/bookController.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../models/");
+
+// TODO: Create book route/ POST Route(save a book)
+
+
+
+// TODO: Delete book route/ DELETE Route(delete a book by id)
+
+
+// TODO: Get books route/ GET Route(get all saved books)
+
