af7c3d405563a1d6a395594776b9c13b4fad44d4
Jason Ozulumba <cjozulumba@gmail.com>
2020-11-07 15:53:12 -0500

    Added Axios and react-router-dom to client node modules

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("client/build"));
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/what-book", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -32,6 +34,10 @@ app.get("/api/config", (req, res) => {
     success: true,
   });
 });
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`);
